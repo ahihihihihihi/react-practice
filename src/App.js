9258a388ee799b0f91a10d852db5982d74577023
ahihi <bdsthocu20@gmail.com>
2023-09-26 11:42:31 +0700

    #4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
 import Header from './components/Header';
 import TableUser from './components/TableUser';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div>
       <Header />
-      <TableUser />
+      <Container>
+        <TableUser />
+      </Container>
     </div>
   );
 }
--- src/components/TableUser.js
+++ src/components/TableUser.js
@@ -1,12 +1,52 @@
-import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import { fetchAllUser } from '../services/UserService';
+import { useEffect, useState } from 'react';
 
 const TableUser = (props) => {
 
+    useEffect(() => {
+        getUsers()
+    }, [])
+
+    const [listUsers, setListUsers] = useState([])
+
+    const getUsers = async () => {
+        let res = await fetchAllUser();
+        if (res && res.data && res.data.data) {
+            setListUsers(res.data.data)
+        }
+        // console.log(">>> check res: ", res)
+    }
+
+    // console.log(">>> check listUsers: ", listUsers)
+
     return (
         <>
-            <Container>
-                <div>Table user...</div>
-            </Container>
+            <Table striped hover bordered>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listUsers && listUsers.length > 0 &&
+                        listUsers.map((item, index) => {
+
+                            return (
+                                <tr key={`users-${index}`}>
+                                    <td>{item.id}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.first_name}</td>
+                                    <td>{item.last_name}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </Table>
         </>
     )
 }

060a9496bb2d8d955f0ffafa4410eb7ffba8bbc1
ahihi <bdsthocu20@gmail.com>
2023-09-26 10:59:41 +0700

    #3

---
+++ src/components/TableUser.js
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container';
+
+const TableUser = (props) => {
+
+    return (
+        <>
+            <Container>
+                <div>Table user...</div>
+            </Container>
+        </>
+    )
+}
+
+export default TableUser

3dfa68fe164b10a4688bce3fc76173bc3b201faa
ahihi <bdsthocu20@gmail.com>
2023-09-26 14:46:28 +0700

    #7

--- src/components/TableUser.js
+++ src/components/TableUser.js
@@ -1,24 +1,34 @@
 import Table from 'react-bootstrap/Table';
 import { fetchAllUser } from '../services/UserService';
 import { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
 const TableUser = (props) => {
 
     useEffect(() => {
-        getUsers()
+        getUsers(1)
     }, [])
 
     const [listUsers, setListUsers] = useState([])
+    const [totalUsers, setTotalUsers] = useState(0)
+    const [totalPages, setTotalPages] = useState(0)
 
-    const getUsers = async () => {
-        let res = await fetchAllUser();
+    const getUsers = async (page) => {
+        let res = await fetchAllUser(page);
         if (res && res.data) {
             setListUsers(res.data)
+            setTotalUsers(res.total)
+            setTotalPages(res.total_pages)
         }
         // console.log(">>> check res: ", res)
     }
 
     // console.log(">>> check listUsers: ", listUsers)
+
+    const handlePageClick = (event) => {
+        // console.log(">>>check event: ", event)
+        getUsers(+event.selected + 1)
+    }
 
     return (
         <>
@@ -47,6 +57,27 @@ const TableUser = (props) => {
                     }
                 </tbody>
             </Table>
+            {/* https://codepen.io/monsieurv/pen/abyJQWQ */}
+            <ReactPaginate
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={2}
+                pageCount={totalPages}
+                previousLabel="< previous"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakLabel="..."
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active"
+                renderOnZeroPageCount={null}
+            />
         </>
     )
 }
